a5d951419ed1b458d60932af5aa4fd07479ef361
iyanyanyan <medianamsp05@gmail.com@gmail.com>
2023-04-12 14:46:35 +0700

    test repo

---
+++ Cilik/__main__.py
@@ -0,0 +1,57 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+# Cilik-PyroBot
+
+from pyrogram import idle
+from uvloop import install
+
+from config import BOT_VER
+from Cilik import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
+from Cilik.helpers.misc import create_botlog, git, heroku
+
+MSG_ON = """
+✅ **Iyan Store - Ubot Activated.**
+**🏷️ Userbot Version -** `{}`
+**Ketik** `.alive` **untuk Mengecheck Bot**
+"""
+
+
+async def main():
+    for bot in bots:
+        try:
+            await bot.start()
+            bot.me = await bot.get_me()
+            await bot.join_chat("matstoreid")
+            await bot.join_chat("judulkuliah")   
+            await bot.join_chat("Asistenyanj")
+            await bot.join_chat("sherleanshop")         
+            try:
+                await bot.send_message(
+                    BOTLOG_CHATID, MSG_ON.format(BOT_VER)
+                )
+            except BaseException:
+                pass
+            LOGGER("IyanStore").info(
+                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
+            )
+        except Exception as a:
+            LOGGER("main").warning(a)
+    LOGGER("IyanStore").info(f"IyanStore-Ubot v{BOT_VER} ⚙️[⚡ Activated ⚡]")
+    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+        await create_botlog(bot1)
+    await idle()
+    await aiosession.close()
+
+
+if __name__ == "__main__":
+    LOGGER("IyanStore").info("Starting IyanStore Bot")
+    install()
+    git()
+    heroku()
+    LOOP.run_until_complete(main())
---
+++ README.md
@@ -0,0 +1,44 @@
+## Iyan uBot
+<h1 align="center"> 
+    Iyan uBot
+</h1>
+
+<p align="center">
+  <a href="#"><img src="https://telegra.ph//file/477aeb3520a27723f9f0d.jpg" width="300" height="300"></a> </br>
+</p>
+
+## Disclaimer
+
+```
+Saya tidak bertanggung jawab atas penyalahgunaan bot ini.
+Bot ini dimaksudkan untuk bersenang-senang sekaligus membantu anda
+mengelola grup secara efisien dan mengotomatiskan beberapa hal yang membosankan.
+Gunakan bot ini dengan risiko Anda sendiri, dan gunakan userbot ini dengan bijak.
+```
+
+
+<h3 align="center">Klik Tombol di Bawah ini untuk Deploy di Heroku</h3>
+<a href="https://dashboard.heroku.com/new?button-url=https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FIyanyanyan%2FIyan-Userbot&template=https%3A%2F%2Fgithub.com%2FIyanyanyan%2FIyan-Userbot="https://www.herokucdn.com/deploy/button.svg"></a>
+</div>
+
+
+
+## Config Vars 📑
+
+- `APP_ID` - Your APP ID. Get it from [my.telegram.org](my.telegram.org)
+- `API_HASH` - Your API_HASH. Get it from [my.telegram.org](my.telegram.org)
+- `HEROKU_APP_NAME` - Your Heroku app name (Only needed if you're deploying this on Heroku)
+- `HEROKU_API_KEY` - Your Heroku API KEY. Get it from [here](https://dashboard.heroku.com/account). (Only needed if you're deploying this on Heroku)
+
+## 👨🏻‍💻 Credits
+
+-  [TeamDerUntergang](https://github.com/TeamDerUntergang/Telegram-SedenUserBot) : SedenUserBot
+-  [TheHamkerCat](https://github.com/TheHamkerCat/WilliamButcherBot) : WilliamButcherBot
+-  [TeamYukki](https://github.com/TeamYukki/YukkiMusicBot) : YukkiMusicBot
+
+## 📑 License
+This repository is license under [GPL-3 License](https://github.com/grey423/Cilik-Ubot/blob/master/LICENSE)
+<p align="Left">
+  </br>
+  <b>Copyright (c) 2022 - iyanstore | Iyan-Ubot</b>
+</p>
